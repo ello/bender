5c1541a69fa05d84d9fdc1237d91ece595ae3d7d
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-03-20 16:32:14 -0600

    Delay creating app until persisted and add cancel

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import { AsyncStorage } from 'react-native'
+import React, { PureComponent } from 'react'
+import { AsyncStorage, Text, View } from 'react-native'
 import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
 import immutableTransform from 'redux-persist-transform-immutable'
 import store from './src/store'
 import AppContainer from './src/containers/AppContainer'
 
-const App = () =>
-  <Provider store={store}>
-    <AppContainer />
-  </Provider>
-
 const whitelist = ['authentication', 'editor', 'gui', 'json', 'profile']
-persistStore(store, { storage: AsyncStorage, transforms: [immutableTransform()], whitelist })
+export default class App extends PureComponent {
 
-export default App
+  state = {
+    rehydrated: false,
+  }
+
+  componentWillMount() {
+    persistStore(
+      store,
+      { storage: AsyncStorage, transforms: [immutableTransform()], whitelist },
+      () => this.setState({ rehydrated: true }),
+    )
+  }
+
+  render() {
+    return this.state.rehydrated ?
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+      :
+      <View><Text>Loading...</Text></View>
+  }
+}
 
