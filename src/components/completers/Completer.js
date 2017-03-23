ef3778a6f93e6a5b6d9889ac7c1b0f8591e457ff
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-03-23 15:55:47 -0600

    WIP: styling the completers

--- src/components/completers/Completer.js
+++ src/components/completers/Completer.js
@@ -1,24 +1,68 @@
 import React, { PropTypes, PureComponent } from 'react'
-import { Text, View } from 'react-native'
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
 
 export const emojiRegex = /\s?:{1}(\w+|\+|-):{0}$/
 export const userRegex = /(\s|^)@{1}\w+/
 
+const scrollViewStyle = {
+  maxHeight: 100,
+}
+
+const completionStyle = {
+  backgroundColor: '#000',
+  borderBottomWidth: 1,
+  borderBottomColor: '#fff',
+  flexDirection: 'row',
+  height: 40,
+  padding: 10,
+}
+
+const imageStyle = {
+  borderRadius: 10,
+  height: 20,
+  width: 20,
+}
+
+const textStyle = {
+  color: '#fff',
+  marginLeft: 10,
+}
+
 export default class Completer extends PureComponent {
 
   static propTypes = {
-    completions: PropTypes.object.isRequired,
+    completions: PropTypes.object,
+    isCompleterActive: PropTypes.bool.isRequired,
+    onCompletion: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    completions: null,
   }
 
   render() {
-    const { completions } = this.props
-
+    const { completions, isCompleterActive } = this.props
+    console.log('COMPLETER height', Dimensions.get('window').height)
+    if (!isCompleterActive || !completions || !completions.get('data').size) { return null }
     return (
-      <View style={{ position: 'absolute' }}>
+      <ScrollView style={scrollViewStyle}>
         {completions.get('data').map(completion =>
-          <Text key={`completion_${completion.get('name')}`}>{completion.get('name')}</Text>,
+          <TouchableOpacity
+            key={`completion_${completion.get('name')}`}
+            onPress={() => this.props.onCompletion({ value: completion.get('name') })}
+            style={completionStyle}
+          >
+            <Image source={{ uri: completion.get('imageUrl') }} style={imageStyle} />
+            <Text style={textStyle}>{completion.get('name')}</Text>
+          </TouchableOpacity>,
         )}
-      </View>
+      </ScrollView>
     )
   }
 }
