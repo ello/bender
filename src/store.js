a1b42838e065859efcda4bf8113684172d192296
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-03-28 16:13:36 -0600

    Bunch opf small tweaks to the completer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { combineReducers, compose, createStore, applyMiddleware } from 'redux'
-// import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import { autoRehydrate } from 'redux-persist'
 import createSagaMiddleware, { END } from 'redux-saga'
 import * as reducers from './reducers'
@@ -17,18 +17,20 @@ const createNativeAppStore = (initialState = {}) => {
   logConfig.stateTransformer = (state) => {
     const newState = {}
     Object.keys(state).forEach((key) => {
-      newState[key] = state[key].toJS()
+      if ([].includes(key)) {
+        newState[key] = state[key].toJS()
+      }
     })
     return newState
   }
-  // const logger = createLogger(logConfig)
+  const logger = createLogger(logConfig)
   const sagaMiddleware = createSagaMiddleware()
 
   const store = compose(
     autoRehydrate(),
     applyMiddleware(
       sagaMiddleware,
-      // logger,
+      logger,
     ),
   )(createStore)(reducer, initialState)
   store.close = () => store.dispatch(END)
