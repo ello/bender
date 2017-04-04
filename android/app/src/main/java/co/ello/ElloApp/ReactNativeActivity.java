ebf3c560657dea068622019d89a0618505b190b8
Jay Zeschin <jayzeschin@Couloir.local>
2017-04-03 20:56:49 -0600

    Spike out sending auth state to React Native

--- android/app/src/main/java/co/ello/ElloApp/ReactNativeActivity.java
+++ android/app/src/main/java/co/ello/ElloApp/ReactNativeActivity.java
@@ -1,8 +1,16 @@
 package co.ello.ElloApp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 
 public class ReactNativeActivity extends ReactActivity {
+
+    String authJSON;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -12,4 +20,27 @@ public class ReactNativeActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "Ello";
     }
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        authJSON = intent.getExtras().getString("AUTH_JSON");
+        Log.d("AUTH JSON", authJSON);
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Nullable
+            @Override
+            protected Bundle getLaunchOptions() {
+                Bundle initialProps = new Bundle();
+                initialProps.putString("AUTH_JSON", authJSON);
+                return initialProps;
+            }
+        };
+    }
+
 }
--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -3,15 +3,7 @@ import Immutable from 'immutable'
 import { REHYDRATE } from 'redux-persist/constants'
 import { AUTHENTICATION, PROFILE } from '../constants/action_types'
 
-export const initialState = Immutable.Map({
-  accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJpc3MiOiJFbGxvLCBQQkMiLCJpYXQiOjE0OTEwMDI5ODAsImp0aSI6IjQwYjcyMzU3YTI0MDY4NDg4ZmRhYjAwMGRlNmQ5Y2EzIiwiZXhwIjoxNDkxMDEwMTgwLCJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6InRlc3RqYXl6ZXMiLCJhbmFseXRpY3NfaWQiOiIxMWIwMjY1NTM0MTk0NjkwNGMzNmJkNmViNzVmOThkMzAyYzgyZThlIiwid2ViX29uYm9hcmRpbmdfdmVyc2lvbiI6IjIiLCJhbGxvd3NfYW5hbHl0aWNzIjp0cnVlLCJjcmVhdGVkX2F0IjoiMjAxNC0xMC0wMVQwNDoxMjowMy4yNjE0MTcwMDArMDAwMCIsImlzX3N0YWZmIjp0cnVlfX0.O6hbTxDbytljepPrr0LxQpmJ1nFNPW0OKP7omvUO0sgiYA0J7Ka5a7E_IVlFqBGgtBPvF93oqgSJHgPDGxQzLQhXaGf4H1ir4yBG5-GHP7_ezB1HYd2v_BxvtjpkwF_pWUUU-mOb5n2jRqHz4R4I6JKhE9ivgS1mWKKXcuoRIAoMYyXjShC_K0tNtF7qh4s2dQRNHZyfpxrhjkvlCJEun8ov5kIhShtzR3_CkmnlW_S_10B_VRs04obwnrg4y8_9h_siD8ZLHtBBnRzUpgPLuImflL8RundfYiL8QevRpkfiaBg8iTL0sqb76sOSb33Gi3ijeoK8u3PYLkknvU_Nvg',
-  createdAt: 1491002980,
-  expirationDate: new Date('Fri Mar 31 2017 19:29:40 GMT-0600 (MDT)'),
-  expiresIn: 7200,
-  isLoggedIn: true,
-  refreshToken: '8bfa643ce1c4417c860d2ac6084d5385940bd30881227da27837a833127d7a07',
-  tokenType: 'bearer',
-})
+export const initialState = Immutable.Map({})
 
 export default (state = initialState, action) => {
   let auth
