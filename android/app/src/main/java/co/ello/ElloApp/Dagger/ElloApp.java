a3fa046f4fdc1731d11a2b579646f0d0962875f1
Sean Dougherty <seancdougherty@gmail.com>
2017-04-05 18:11:18 -0600

    Prevent editor from crashing app

--- android/app/src/main/java/co/ello/ElloApp/Dagger/ElloApp.java
+++ android/app/src/main/java/co/ello/ElloApp/Dagger/ElloApp.java
@@ -2,7 +2,20 @@ package co.ello.ElloApp.Dagger;
 
 import android.app.Application;
 
-public class ElloApp extends Application {
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.imagepicker.ImagePickerPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import co.ello.ElloApp.BuildConfig;
+
+public class ElloApp extends Application implements ReactApplication {
 
     private NetComponent netComponent;
 
@@ -14,6 +27,28 @@ public class ElloApp extends Application {
                 .appModule(new AppModule(this))
                 .netModule(new NetModule(this))
                 .build();
+
+        SoLoader.init(this, /* native exopackage */ false);
+    }
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new ReactNativeConfigPackage(),
+                    new ImagePickerPackage()
+            );
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     public NetComponent getNetComponent() {
