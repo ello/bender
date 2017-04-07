51f91a6aac4130f24f5fefa9a74ab173ec97c2f7
Sean Dougherty <seancdougherty@gmail.com>
2017-04-07 15:42:08 -0600

    Handle GCM from ReactNativeActivity

--- android/app/src/main/java/co/ello/ElloApp/ElloPreferences.java
+++ android/app/src/main/java/co/ello/ElloApp/ElloPreferences.java
@@ -4,8 +4,8 @@ public class ElloPreferences {
 
     private final static String TAG = ElloPreferences.class.getSimpleName();
 
+    public static final String PUSH_PATH_FROM_REACT = "pushPathFromReact";
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
-    public static final String IMAGE_RESIZED = "imageResized";
     public static final String PUSH_RECEIVED = "pushReceived";
 }
