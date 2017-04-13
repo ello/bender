1bddecc1dd03c083734da1ad99fedaccde178176
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-04-13 16:49:15 -0600

    Adds native segment analytics track calls
    [Finishes #143616293](https://www.pivotaltracker.com/story/show/143616293)

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,11 @@
 import { fork } from 'redux-saga/effects'
+import analyticsSaga from './analytics'
 import requesterSaga from './requester'
 import uploaderSaga from './uploader'
 
 export default function* root() {
   yield [
+    fork(analyticsSaga),
     fork(requesterSaga),
     fork(uploaderSaga),
   ]
