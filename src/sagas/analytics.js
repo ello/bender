ee30e14b23e3132f164afeff062292f8bf7a11f2
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-04-14 12:17:27 -0600

    Allows more than one track call to go out
    [Finishes #143616293](https://www.pivotaltracker.com/story/show/143616293)

--- src/sagas/analytics.js
+++ src/sagas/analytics.js
@@ -3,9 +3,10 @@ import { actionChannel, fork, put, select, take } from 'redux-saga/effects'
 import { LOCATION_CHANGE } from 'react-router-redux'
 import get from 'lodash/get'
 import * as ACTION_TYPES from '../constants/action_types'
+import { RELATIONSHIP_PRIORITY } from '../constants/relationship_types'
 import { trackEvent as trackEventAction } from '../actions/analytics'
 import { selectActiveNotificationsType } from '../selectors/gui'
-import { selectAnalyticsId } from '../selectors/profile'
+import { selectAnalyticsId, selectProfileIsFeatured } from '../selectors/profile'
 import { analytics as segment } from '../containers/AnalyticsContainer'
 
 let shouldCallInitialTrackPage = false
@@ -33,30 +34,90 @@ function* trackEvent() {
 function* trackEvents() {
   while (true) {
     const action = yield take('*')
-    const method = get(action, 'payload.method')
     switch (action.type) {
-      case ACTION_TYPES.COMMENT.CREATE_REQUEST:
-        return yield put(trackEventAction('published_comment'))
-      case ACTION_TYPES.COMMENT.DELETE_REQUEST:
-        return yield put(trackEventAction('deleted_comment'))
-      case ACTION_TYPES.POST.DELETE_REQUEST:
-        return yield put(trackEventAction('deleted_post'))
-      case ACTION_TYPES.POST.LOVE_REQUEST:
-        if (method === 'POST') {
-          return yield put(trackEventAction('web_production.post_actions_love'))
+      case ACTION_TYPES.ALERT.OPEN:
+      case ACTION_TYPES.MODAL.OPEN:
+        if (get(action, 'payload.trackLabel')) {
+          yield put(trackEventAction(get(action, 'payload.trackLabel')))
         }
         break
-      case ACTION_TYPES.POST.WATCH_REQUEST:
-        if (method === 'DELETE') {
-          return yield put(trackEventAction('unwatched-post'))
+      case ACTION_TYPES.COMMENT.CREATE_REQUEST:
+        yield put(trackEventAction('published_comment'))
+        break
+      case ACTION_TYPES.COMMENT.DELETE_REQUEST:
+        yield put(trackEventAction('deleted_comment'))
+        break
+      case ACTION_TYPES.OMNIBAR.OPEN:
+        yield put(trackEventAction('opened_omnibar'))
+        break
+      case ACTION_TYPES.POST.CREATE_REQUEST: {
+        const isFeatured = yield select(selectProfileIsFeatured)
+        if (get(action, 'payload.body.body[0].link_url', '').length) {
+          yield put(trackEventAction('added_buy_button'))
         }
-        return yield put(trackEventAction('watched-post'))
+        yield put(trackEventAction(get(action, 'meta.repostId') ? 'published_repost' : 'published_post', { isFeatured }))
+        break
+      }
+      case ACTION_TYPES.POST.DELETE_REQUEST:
+        yield put(trackEventAction('deleted_post'))
+        break
+      case ACTION_TYPES.POST.LOVE_REQUEST: {
+        const method = get(action, 'payload.method')
+        if (method === 'POST') {
+          yield put(trackEventAction('web_production.post_actions_love'))
+        }
+        break
+      }
+      case ACTION_TYPES.POST.WATCH_REQUEST: {
+        const method = get(action, 'payload.method')
+        if (method === 'DELETE') {
+          yield put(trackEventAction('unwatched-post'))
+          break
+        }
+        yield put(trackEventAction('watched-post'))
+        break
+      }
       case ACTION_TYPES.POST.UPDATE_REQUEST:
-        return yield put(trackEventAction('edited_post'))
+        yield put(trackEventAction('edited_post'))
+        break
       case ACTION_TYPES.PROFILE.DELETE_REQUEST:
-        return yield put(trackEventAction('user-deleted-account'))
+        yield put(trackEventAction('user-deleted-account'))
+        break
+      case ACTION_TYPES.PROFILE.FOLLOW_CATEGORIES_REQUEST:
+        yield put(trackEventAction('Onboarding.Settings.Categories.Completed',
+          { categories: get(action, 'payload.body.followed_category_ids', []).length },
+        ))
+        break
       case ACTION_TYPES.PROFILE.SIGNUP_SUCCESS:
-        return yield put(trackEventAction('join-successful'))
+        yield put(trackEventAction('join-successful'))
+        break
+      case ACTION_TYPES.RELATIONSHIPS.UPDATE_INTERNAL:
+      case ACTION_TYPES.RELATIONSHIPS.UPDATE_REQUEST:
+        switch (get(action, 'payload.priority')) {
+          case RELATIONSHIP_PRIORITY.FRIEND:
+          case RELATIONSHIP_PRIORITY.NOISE:
+            yield put(trackEventAction('followed_user'))
+            break
+          case RELATIONSHIP_PRIORITY.INACTIVE:
+          case RELATIONSHIP_PRIORITY.NONE:
+            yield put(trackEventAction('unfollowed_user'))
+            break
+          case RELATIONSHIP_PRIORITY.BLOCK:
+            yield put(trackEventAction('blocked_user'))
+            break
+          case RELATIONSHIP_PRIORITY.MUTE:
+            yield put(trackEventAction('muted_user'))
+            break
+          default:
+            break
+        }
+        break
+      case ACTION_TYPES.USER.COLLAB_WITH_REQUEST:
+        yield put(trackEventAction('send-collab-dialog-profile'))
+        break
+      case ACTION_TYPES.USER.HIRE_ME_REQUEST:
+        yield put(trackEventAction('send-hire-dialog-profile'))
+        break
       default:
         break
     }
