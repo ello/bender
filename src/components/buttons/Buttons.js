3efce6aa0c227f6048b420cd2edaf8c3fdbf64e7
Matthew Kitt <mk@mkitt.net>
2017-04-17 22:49:50 -0600

    Update buttons and post actions UI
    Based on the latest comps and heading down the material design
    direction. This includes utilizing `TouchableNativeFeedback`as the
    button base. Cleaned up the buttons quite a bit here as well as making
    jso creation (`StyleSheet`) a little more "react native" like.
    
    [#143184185](https://www.pivotaltracker.com/story/show/143184185)

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,77 +1,116 @@
 // @flow
 /* eslint-disable import/prefer-default-export */
-import React from 'react'
+import React, { PureComponent } from 'react'
 // $FlowFixMe
-import { Text, TouchableOpacity, View } from 'react-native'
-import { PencilIcon } from '../assets/Icons'
+import { StyleSheet, TouchableNativeFeedback, View } from 'react-native'
+import type { JSO } from '../../types/flowtypes'
+
+type BaseIconButtonProps = {
+  children: React.Element<*> | null,
+  disabled?: boolean,
+  size: string,
+  styles: {
+    base: JSO,
+    large: JSO,
+    medium: JSO,
+    small: JSO,
+    normal: JSO,
+    disabled: JSO,
+  },
+}
+
+const baseIconButtonStyle = {
+  base: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  large: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+  },
+  medium: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  small: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+  },
+}
+
+class BaseIconButton extends PureComponent {
+  props: BaseIconButtonProps
+
+  static defaultProps = {
+    disabled: false,
+    children: null,
+    size: 'medium',
+  }
+
+  getStyleModifer() {
+    const { disabled } = this.props
+    if (disabled) {
+      return 'disabled'
+    }
+    return 'normal'
+  }
+
+  render() {
+    const { children, disabled, size, styles, ...props } = this.props
+    return (
+      <View style={[styles.base, styles[size], styles[this.getStyleModifer()]]} >
+        <TouchableNativeFeedback background={TouchableNativeFeedback.Ripple('rgba(0, 0, 0, .12)', true)} delayPressIn={0} disabled={disabled} {...props} >
+          <View style={[styles.base, styles[size]]}>
+            {children}
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    )
+  }
+}
+
+// -------------------------------------
 
 type Props = {
   children: React.Element<*>,
-  disabled?: boolean,
 }
 
-const disabledOpacityStyle = {
-  opacity: 0.2,
+const floatingButtonStyles = {
+  ...baseIconButtonStyle,
+  normal: {
+    elevation: 4,
+    backgroundColor: '#00d100',
+
+  },
+  disabled: {
+    elevation: 0,
+    opacity: 0.4,
+    backgroundColor: 'transparent',
+  },
 }
+
+export const FloatingButton = ({ children, ...props }: Props) => (
+  <BaseIconButton styles={floatingButtonStyles} {...props} >
+    {children}
+  </BaseIconButton>
+)
 
 // -------------------------------------
 
-const iconButtonStyle = {
-  justifyContent: 'center',
-  alignItems: 'center',
-  minWidth: 44,
-  minHeight: 44,
-  marginRight: 5,
-}
+const iconButtonStyles = StyleSheet.create({
+  ...baseIconButtonStyle,
+  normal: {},
+  disabled: {
+    opacity: 0.4,
+  },
+})
 
-export const IconButton = ({ children, disabled, ...props }: Props) =>
-  <TouchableOpacity style={iconButtonStyle} disabled={disabled} {...props} >
-    <View style={disabled && disabledOpacityStyle}>
-      {children}
-    </View>
-  </TouchableOpacity>
-
-IconButton.defaultProps = {
-  disabled: false,
-}
-
-// -------------------------------------
-
-const postButtonStyle = {
-  flex: 1,
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
-  minWidth: 44,
-  minHeight: 44,
-  paddingHorizontal: 40,
-  backgroundColor: 'black',
-}
-
-const postButtonTextStyleBase = {
-  marginLeft: 5,
-  fontSize: 14,
-}
-
-const postButtonTextStyle = {
-  ...postButtonTextStyleBase,
-  color: '#fff',
-}
-
-const postButtonTextStyleDisabled = {
-  ...postButtonTextStyleBase,
-  color: '#aaa',
-}
-
-export const PostButton = ({ children, disabled, ...props }: Props) =>
-  <TouchableOpacity style={postButtonStyle} disabled={disabled} {...props} >
-    <PencilIcon disabled={disabled} />
-    <Text style={disabled ? postButtonTextStyleDisabled : postButtonTextStyle}>
-      {children}
-    </Text>
-  </TouchableOpacity>
-
-PostButton.defaultProps = {
-  disabled: false,
-}
+export const IconButton = ({ children, ...props }: Props) => (
+  <BaseIconButton styles={iconButtonStyles} {...props} >
+    {children}
+  </BaseIconButton>
+)
 
