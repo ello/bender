cb2251efccf6e07e644b94f82fa14d35954db7b7
Sean Dougherty <seancdougherty@gmail.com>
2017-05-17 15:39:40 -0600

    autofocus the TextInput

--- src/components/editor/TextBlock.js
+++ src/components/editor/TextBlock.js
@@ -60,6 +60,7 @@ export default class TextBlock extends PureComponent {
           <TextInput
             defaultValue={data}
             multiline
+            autoFocus="true"
             onBlur={this.context.onHideCompleter}
             onFocus={this.onFocus}
             onChangeText={this.onChangeText}
