0e9fe56388a011531c89b23a1fe8e5c22d4b6e0d
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-05-19 13:59:47 -0600

    Fix a warning with autoFocus

--- src/components/editor/TextBlock.js
+++ src/components/editor/TextBlock.js
@@ -60,7 +60,7 @@ export default class TextBlock extends PureComponent {
           <TextInput
             defaultValue={data}
             multiline
-            autoFocus="true"
+            autoFocus
             onBlur={this.context.onHideCompleter}
             onFocus={this.onFocus}
             onChangeText={this.onChangeText}
